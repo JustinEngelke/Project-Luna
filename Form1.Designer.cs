223f6a255bf47f2a38a25a3cf8945d8b695b46b9
JustinEngelke <justin.engelkejn@gmail.com>
2017-09-29 13:23:57 +0200

    Fixed issue with upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ProjectLuna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnVoice = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVoice
+            // 
+            this.btnVoice.BackColor = System.Drawing.Color.Transparent;
+            this.btnVoice.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnVoice.BackgroundImage")));
+            this.btnVoice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnVoice.FlatAppearance.BorderSize = 0;
+            this.btnVoice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoice.Location = new System.Drawing.Point(12, 12);
+            this.btnVoice.Name = "btnVoice";
+            this.btnVoice.Size = new System.Drawing.Size(43, 48);
+            this.btnVoice.TabIndex = 0;
+            this.btnVoice.UseVisualStyleBackColor = false;
+            this.btnVoice.Click += new System.EventHandler(this.btnVoice_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(-3, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 70);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnVoice);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVoice;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Diagnostics;
+using System.IO;
+
+namespace ProjectLuna
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine LunaBrain = new SpeechRecognitionEngine();
+        SpeechSynthesizer LunaVoice = new SpeechSynthesizer();
+        bool trigger = false ;
+        string User = Environment.UserDomainName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVoice_Click(object sender, EventArgs e)
+        {
+            if (trigger == false)
+            {
+                LunaBrain.RecognizeAsync(RecognizeMode.Multiple);
+                trigger = true;
+                btnVoice.BackgroundImage = ((System.Drawing.Image)(Properties.Resources.micActive));
+            }
+            else {
+                LunaBrain.RecognizeAsyncStop();
+                btnVoice.BackgroundImage = ((System.Drawing.Image)(Properties.Resources.micOff));
+                trigger = false;
+            }
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Basic Commands for Luna 
+            //Access Luna
+            Choices BasicuserCommands = new Choices();
+            BasicuserCommands.Add(new string[] {"Hello Luna","Goodbye Luna"});
+            GrammarBuilder basicActionGrammar = new GrammarBuilder();
+            basicActionGrammar.Append(BasicuserCommands);
+            Grammar basicGrammar = new Grammar(basicActionGrammar);
+            LunaBrain.LoadGrammarAsync(basicGrammar);
+
+            //Disable mircophone
+            Choices microphone = new Choices();
+            microphone.Add(new string[] { "Disable Mic" });
+            GrammarBuilder microphoneGrammarBuilder = new GrammarBuilder();
+            microphoneGrammarBuilder.Append(microphone);
+            Grammar microphoneGrammar = new Grammar(microphoneGrammarBuilder);
+            LunaBrain.LoadGrammarAsync(microphoneGrammar);
+
+            //Commands that involve the pc
+            Choices pcCommands = new Choices();
+            pcCommands.Add(new string[] { "Open Notepad" });
+            GrammarBuilder pcGrammarBuilder = new GrammarBuilder();
+            pcGrammarBuilder.Append(pcCommands);
+            Grammar pcGrammar = new Grammar(pcGrammarBuilder);
+            LunaBrain.LoadGrammarAsync(pcGrammar);
+
+
+
+
+            LunaBrain.SetInputToDefaultAudioDevice();
+            LunaBrain.SpeechRecognized += LunaBrain_SpeechRecognized;
+            LunaVoice.Rate = 0;
+            LunaVoice.SelectVoiceByHints(VoiceGender.Female);
+
+            
+
+        }
+
+        private void LunaBrain_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string greeting = "";
+            string time = "";
+            int hour = 0;
+            DateTime date1 = DateTime.Now ;           
+            time = date1.ToString("tt");
+            hour = int.Parse( date1.ToString("%h"));
+
+
+
+            switch (e.Result.Text)
+            {
+                case "Hello Luna":
+                    
+                    PromptBuilder builder = new PromptBuilder();
+                    if (time == "PM" && hour >=12 && hour < 18) {
+                        greeting = "Good Afternoon";
+                    }else
+                    {
+                        greeting = "Good Evening";
+                    }
+                     if (time == "AM")
+                    {
+                        greeting = "Good Morning";
+                    };
+
+                    builder.AppendText(greeting + " " + User);
+
+                    
+                    LunaVoice.Speak(builder);
+                    break;
+
+                case "Goodbye Luna":
+                    PromptBuilder Goodbye = new PromptBuilder();
+                    Goodbye.AppendText("Goodbye " + User);
+                    LunaVoice.Speak(Goodbye);
+                    Application.Exit();
+
+                    break;
+
+                case "Open Notepad":
+                    System.Diagnostics.Process.Start(@"C:\Windows\System32\Notepad.exe");
+                    PromptBuilder open = new PromptBuilder();
+                    open.AppendText("Opening Notepad ");
+                    LunaVoice.Speak(open);
+
+                    break;
+
+                case "Disable Mic":
+                    LunaBrain.RecognizeAsyncStop();
+                    PromptBuilder off = new PromptBuilder();
+                    off.AppendText("Your microphone is now disabled!");
+                    LunaVoice.Speak(off);
+                    btnVoice.BackgroundImage = ((System.Drawing.Image)(Properties.Resources.micOff));
+                    trigger = false;
+                    break;
+                    
+            }
+        }
+    }
+}
